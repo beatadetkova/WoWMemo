4a6a60b11b7f43377e1005acf349337808081747
Beata Detkova <beata.detkova.jr@gmail.com>
2020-08-12 19:04:35 +0200

    Transform function to class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+import Calendar from "./components/Calendar";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div id="logo">
+            <span>
+              <b>WoWMemo</b>
+            </span>
+          </div>
+        </header>
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
