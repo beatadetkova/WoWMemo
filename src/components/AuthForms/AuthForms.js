e364e258527e110ea47bbc6fbe941327d07d462a
Beata Detkova <beata.detkova.jr@gmail.com>
2020-08-25 19:08:07 +0200

    Created collection of styled-components

---
+++ src/components/AuthForms/AuthForms.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  box-sizing: border-box;
+  max-width: 410px;
+  margin: 0 auto;
+  padding: 0 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Welcome = styled.div`
+  display: block;
+  width: 100%;
+  text-align: center;
+  font-weight: bold;
+  font-size: 40px;
+  max-width:500px;
+  padding:1em;
+  background: linear-gradient(to right, #e65c00, #F9D423);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-family: Friz Quadrata Regular OS;
+`;
+
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 1rem;
+  border: 1px solid #999;
+  margin-bottom: 1rem;
+  font-size: 0.8rem;
+  font-family: Friz Quadrata Regular OS;
+`;
+
+const Button = styled.button`
+  background: linear-gradient(to bottom, #6371c7, #5563c1);
+  border-color: #3f4eae;
+  border-radius: 3px;
+  padding: 1rem;
+  color: white;
+  font-weight: 700;
+  width: 100%;
+  margin-bottom: 1rem;
+  font-size: 0.8rem;
+  font-family: Friz Quadrata Regular OS;
+`;
+
+
+const Error = styled.div`
+  color: red;
+  font-family: Friz Quadrata Regular OS;
+`;
+
+export { Form, Input, Button, Card, Welcome, Error };
