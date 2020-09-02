72a7e50c221cdc149c3c54ffe77f67dd1705c5aa
Beata Detkova <beata.detkova.jr@gmail.com>
2020-09-02 18:43:58 +0200

    Implemented TaskManagerToggler into Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
 import TaskManager from "../TaskManager/TaskManager.js"
+import TaskManagerToggler from "../TaskManagerToggler/TaskManagerToggler.js"
 
 function Home(props) {
-  return   <div className="App">
+
+  const [taskManagerActive, setTaskManagerActive] = useState(false);
+
+  const toggleTaskManager = () => {
+    setTaskManagerActive(!taskManagerActive);
+  }
+
+  return <div className="App">
       <header className="App-header">
       WoWMemo
       </header>
       <main>
-        <TaskManager />
+        {taskManagerActive && <TaskManager />}
       </main>
+      <div>
+        <TaskManagerToggler toggle={ toggleTaskManager }/>
+      </div>
     </div>
 }
 
