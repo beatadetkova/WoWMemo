23f261b580ca73909f392b95678e09ee4307e939
Beata Detkova <beata.detkova.jr@gmail.com>
2020-09-03 19:07:43 +0200

    Updated layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TaskManager from "../TaskManager/TaskManager.js"
-import TaskManagerToggler from "../TaskManagerToggler/TaskManagerToggler.js"
+import './dist/Home.css'
 
 function Home(props) {
-
-  const [taskManagerActive, setTaskManagerActive] = useState(false);
-
-  const toggleTaskManager = () => {
-    setTaskManagerActive(!taskManagerActive);
-  }
 
   return <div className="App">
       <header className="App-header">
       WoWMemo
       </header>
       <main>
-        {taskManagerActive && <TaskManager />}
+        <div className="selected-date">
+          <TaskManager />
+        </div>
+        <label className="recurring">Recurring:</label>
+        <div className="recurring">
+          <div className="daily">
+            <div>
+              <label>Daily:</label> 
+              <TaskManager type="daily"/>
+            </div>
+          </div>
+          <div className="weekly">
+            <div>
+              <label>Weekly:</label>
+              <TaskManager type="weekly"/>
+            </div>
+          </div>
+        </div>
       </main>
-      <div>
-        <TaskManagerToggler toggle={ toggleTaskManager }/>
-      </div>
     </div>
 }
 
