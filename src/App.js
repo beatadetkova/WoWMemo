2c14593a16301bb1830af6a239343dfd8d2e45ef
Beata Detkova <beata.detkova.jr@gmail.com>
2020-09-24 19:56:41 +0200

    Quick Redux test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
-import Home from './componenerrorts/Home/Home.js';
+import Home from './components/Home/Home.js';
 import { AuthContext } from './context/Auth.js';
 import Login from './components/Login/Login.js';
 import Signup from './components/SignUp/SignUp.js';
 import './dist/App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTask } from './redux/actions';
 
 function App(props) {
+  const dispatch = useDispatch();
   const [authTokens, setAuthTokens] = useState();
+  const state = useSelector((state) => state);
+  console.log(state);
+
+  useEffect(() => {
+    dispatch(addTask());
+  }, [dispatch]);
 
   const setTokens = (tokens) => {
     localStorage.setItem('tokens', JSON.stringify(tokens));
